74794ac92bf861c93f5f84b08315d737689e1dd3
Avinash Mandalapu <Avinashchowdhary123@gmail.com>
2023-11-07 23:02:50 +0530

    Added update Page path to urls

--- contacts/urls.py
+++ contacts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('create/', views.contact_create, name='contact_create'),
     path('<int:pk>/', views.contact_detail, name='contact_detail'),
     path('<int:pk>/delete/', views.contact_delete, name='contact_delete'),
+    path('<int:pk>/update/', views.contact_update, name='contact_update'),
 ]
