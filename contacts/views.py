89819ad4051a8d72e8ab6d7b788ce069dea4d18f
Avinash Mandalapu <Avinashchowdhary123@gmail.com>
2023-11-07 22:53:08 +0530

    create Contact List view

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Contact
 
-# Create your views here.
+
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contacts/contact_list.html', {'contacts': contacts})
