0af2cd4c417056dc26d92089f1004b49b9897cbf
Avinash Mandalapu <Avinashchowdhary123@gmail.com>
2023-11-07 22:59:48 +0530

    Added Delete View, added path to url

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.contact_list, name='contact_list'),
     path('create/', views.contact_create, name='contact_create'),
     path('<int:pk>/', views.contact_detail, name='contact_detail'),
+    path('<int:pk>/delete/', views.contact_delete, name='contact_delete'),
 ]
